cf87bc71e0f0decb88a468e115b59ec43499b079
haj300 <47028075+haj300@users.noreply.github.com>
2025-02-10 22:43:30 +0100

    refactor: simplify App.svelte by removing unused image imports and styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,9 @@
 <script>
-  import croppedImg from "./assets/crop.jpg";
-  import bigImg from "/big.jpg";
-  import uddImg from "./assets/udd.jpg";
-  import "./App.css";
+  import "./app.css";
 </script>
 
 <main>
   <div class="body">
-    <!-- <a href="" target="_blank" rel="noreferrer">
-        <img src={bigImg} class="logo" alt="Vite Logo" />
-      </a>
-      <a href="" target="_blank" rel="noreferrer">
-        <img src={uddImg} class="logo svelte" alt="Svelte Logo" />
-      </a> -->
     <h1>ELSA BINDER</h1>
     <h2>KOMMER SNART</h2>
     <p>binder.elsa@gmail.com</p>
@@ -20,18 +11,6 @@
 </main>
 
 <style>
-  .logo {
-    height: 20em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
   .body {
     background-color: aliceblue;
     padding: 20rem;
